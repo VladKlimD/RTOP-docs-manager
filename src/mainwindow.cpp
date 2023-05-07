f1fe35b8abe07d4dfd35c2e9f3d63514cfb78747
VladKlimD <96782197+VladKlimD@users.noreply.github.com>
2023-05-07 20:38:43 +0300

    Проект переведен на cmake

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include "style.hpp"
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    setStyleSheet(STYLE_SPYBOT_THEME);
 }
 
 MainWindow::~MainWindow()
